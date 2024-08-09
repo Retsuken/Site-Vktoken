2d69c7830c69d1e8bd0f84cc6bdb7246a930d98f
Retsuken <151532753+Retsuken@users.noreply.github.com>
2024-08-10 04:28:36 +0500

    Update settings.py

--- maincource3/maincource3/settings.py
+++ maincource3/maincource3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aipw&kiv_prag%!oa8-ll-fqv0nc_e0e*ojk)(6(3bv-@6v7e@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
